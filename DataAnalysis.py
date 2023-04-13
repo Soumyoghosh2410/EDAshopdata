e04511204e78360ad3b286a11eda8f91992685e0
Soumyo Ghosh <soumyo.ghosh24102003@gmail.com>
2023-04-13 17:32:45 +0530

    Add files via upload

--- DataAnalysis.py
+++ DataAnalysis.py
@@ -73,6 +73,192 @@ test_data['Item_Fat_Content']=train_data['Item_Fat_Content'].replace('LF', 'low 
 test_data['Item_Fat_Content']=train_data['Item_Fat_Content'].replace('Low Fat', 'low fat')
 test_data['Item_Fat_Content']=train_data['Item_Fat_Content'].replace('reg', 'Regular')
 
+#OMITTING MISSING VALUES
+
+train_data_omit=train_data.dropna(axis=0)
+
+train_data_omit=pd.get_dummies(train_data_omit, drop_first=True)
+
+
+test_data_omit=train_data.dropna(axis=0)
+
+test_data_omit=pd.get_dummies(train_data_omit, drop_first=True)
+
+from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error
+
+
+x1=train_data_omit.drop(['Item_Outlet_Sales'], axis='columns', inplace=False)
+y1=train_data_omit['Item_Outlet_Sales']
+x2=test_data_omit.drop(['Item_Outlet_Sales'], axis='columns', inplace=False)
+y2=test_data_omit['Item_Outlet_Sales']
+
+#finding mean of test data value
+base_pred=np.mean(y2)
+print(base_pred)
+
+base_pred=np.repeat(base_pred, len(y2))
+
+#LINEAR REGRESSION WITH OMITTED DATA
+
+lgr=LinearRegression(fit_intercept=True)
+
+#model
+model_lin1=lgr.fit(x1,y1)
+
+#predicting model on test set
+
+Sales_prediction_lin1=lgr.predict(x2)
+
+#MSE AND RMSE to measure the accuracy of the model
+
+lin_mse1 = mean_squared_error(y2, Sales_prediction_lin1)
+lin_rmse1 = np.sqrt(lin_mse1)
+print(lin_rmse1)
+
+#R-squared value
+
+r2_lin_test1=model_lin1.score(x2,y2)
+r2_lin_train1=model_lin1.score(x1,y1)
+print(r2_lin_test1,r2_lin_train1)
+
+residuals1=y2-Sales_prediction_lin1
+sns.regplot(x=Sales_prediction_lin1, y=residuals1, scatter=True, 
+            fit_reg=False)
+residuals1.describe()
+
+
+#RANDOM FOREST REGRESSION 
+
+rf = RandomForestRegressor(n_estimators = 100,max_features='auto',
+                           max_depth=100,min_samples_split=10,
+                           min_samples_leaf=4,random_state=1)
+
+# Model
+model_rf1=rf.fit(x1,y1)
+
+# Predicting model on test set
+cars_predictions_rf1 = rf.predict(x2)
+
+# Computing MSE and RMSE
+rf_mse1 = mean_squared_error(y2, cars_predictions_rf1)
+rf_rmse1 = np.sqrt(rf_mse1)
+print(rf_rmse1)
+
+# R squared value
+r2_rf_test1=model_rf1.score(x2,y2)
+r2_rf_train1=model_rf1.score(x1,y1)
+print(r2_rf_test1,r2_rf_train1) 
+
+#BUILDING MODEL WITH IMPUTED DATA
+
+train_data_imputed=train_data.apply(lambda x:x.fillna(x.mean()) \
+                  if x.dtype=='float' else \
+                  x.fillna(x.value_counts().index[0]))
+train_data_imputed.isnull().sum()
+
+test_data_imputed=test_data.apply(lambda x:x.fillna(x.mean()) \
+                  if x.dtype=='float' else \
+                  x.fillna(x.value_counts().index[0]))
+test_data_imputed.isnull().sum()
+
+#CATEGORICAL VARIABLE TO DUMMY VARIABLES
+
+train_data_imputed=pd.get_dummies(train_data_imputed,drop_first=True) 
+test_data_imputed=pd.get_dummies(test_data_imputed,drop_first=True) 
+
+x3=train_data_imputed.drop(['Item_Outlet_Sales'] , inplace=False)
+y3=train_data_imputed['Item_Outlet_Sales']
+x4=test_data_imputed.drop(['Item_Outlet_Sales'] , inplace=False)
+y4=test_data_imputed['Item_Outlet_Sales']
+
+
+
+#finding mean of test data value
+base_pred2=np.mean(y4)
+print(base_pred2)
+
+base_pred=np.repeat(base_pred, len(y4))
+
+#LINEAR REGRESSION WITH OMITTED DATA
+
+lgr2=LinearRegression(fit_intercept=True)
+
+#model
+model_lin2=lgr.fit(x3,y3)
+
+#predicting model on test set
+
+Sales_prediction_lin2=lgr.predict(x4)
+
+#MSE AND RMSE to measure the accuracy of the model
+
+lin_mse2 = mean_squared_error(y4, Sales_prediction_lin2)
+lin_rmse2 = np.sqrt(lin_mse2)
+print(lin_rmse2)
+
+#R-squared value
+
+r2_lin_test2=model_lin2.score(x4,y4)
+r2_lin_train2=model_lin2.score(x3,y3)
+print(r2_lin_test2,r2_lin_train2)
+
+#RANDOM FOREST WITH IMPUTED DATA
+# Model parameters
+rf2 = RandomForestRegressor(n_estimators = 100,max_features='auto',
+                           max_depth=100,min_samples_split=10,
+                           min_samples_leaf=4,random_state=1)
+
+# Model
+model_rf2=rf2.fit(x3,y3)
+
+# Predicting model on test set
+cars_predictions_rf2 = rf2.predict(x4)
+
+# Computing MSE and RMSE
+rf_mse2 = mean_squared_error(y4, cars_predictions_rf2)
+rf_rmse2 = np.sqrt(rf_mse2)
+print(rf_rmse2)
+
+# R squared value
+r2_rf_test2=model_rf2.score(x4,y4)
+r2_rf_train2=model_rf2.score(x3,y4)
+print(r2_rf_test2,r2_rf_train2)     
+
+
+print("Metrics for models built from data where missing values were omitted")
+print("R squared value for train from Linear Regression=  %s"% r2_lin_train1)
+print("R squared value for test from Linear Regression=  %s"% r2_lin_test1)
+print("R squared value for train from Random Forest=  %s"% r2_rf_train1)
+print("R squared value for test from Random Forest=  %s"% r2_rf_test1)
+
+print("RMSE value for test from Linear Regression=  %s"% lin_rmse1)
+print("RMSE value for test from Random Forest=  %s"% rf_rmse1)
+print("\n\n")
+print("Metrics for models built from data where missing values were imputed")
+print("R squared value for train from Linear Regression=  %s"% r2_lin_train2)
+print("R squared value for test from Linear Regression=  %s"% r2_lin_test2)
+print("R squared value for train from Random Forest=  %s"% r2_rf_train2)
+print("R squared value for test from Random Forest=  %s"% r2_rf_test2)
+
+print("RMSE value for test from Linear Regression=  %s"% lin_rmse2)
+print("RMSE value for test from Random Forest=  %s"% rf_rmse2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
